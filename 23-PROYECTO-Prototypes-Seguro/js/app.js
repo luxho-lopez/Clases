2ccc94cca8fc8b2bbd5e5436a92881dfd366e298
luxho-lopez <dj_97@outlook.com>
2023-06-10 22:14:22 -0600

    ✨

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -23,8 +23,38 @@ UI.prototype.llenarOpciones = () => {
 
 // Instanciar UI
 const ui = new UI();
-console.log(ui);
+// console.log(ui);
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); //Llena el select con los años
 });
+
+
+
+eventListener();
+
+function eventListener() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+    
+
+    // Leer el Year seleccionado
+    const year = document.querySelector('#year').value;
+
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if(marca === '' || year === '' || tipo === '') {
+        console.log('Los campos son requeridos');
+    } else {
+        console.log('Validadndo datos');
+    }
+}
