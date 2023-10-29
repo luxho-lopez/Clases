4775d2c5836f9e08c1750101419eec1737f99d88
luxho-lopez <dj_97@outlook.com>
2023-10-28 22:22:30 -0600

    ✨

---
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -0,0 +1,141 @@
+const monedasSelect = document.querySelector('#moneda');
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
+
+// crear Promise
+const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
+    resolve(criptomonedas);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedasSelect.addEventListener('change', leerValor);
+});
+
+function consultarCriptomonedas() {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=15&tsym=USD';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => obtenerCriptomonedas(resultado.Data))
+        .then(criptomonedas => selectCriptomonedas(criptomonedas))
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach(cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('OPTION');
+        option.value = Name;
+        option.textContent = FullName;
+
+        criptomonedasSelect.appendChild(option);
+    })
+}
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+
+    console.log(objBusqueda);
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar el formulario de seleccion
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if (moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son requeridos.');
+        return;
+    }
+
+    // Consultar la API con los balores del formulario
+    consultarApi();
+}
+
+function mostrarAlerta(mensaje) {
+
+    const existeError = document.querySelector('.error');
+
+    if (!existeError) {
+        const divMensaje = document.createElement('DIV');
+        divMensaje.classList.add('error');
+
+        divMensaje.textContent = mensaje;
+
+        formulario.appendChild(divMensaje);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 2000);
+    }
+}
+
+function consultarApi() {
+    const { moneda, criptomoneda } = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    mostrarSpinner();
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarCotizacionHTML(resultado.DISPLAY[criptomoneda][moneda]));
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+    limpiarHTML();
+
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+    const precio = document.createElement('P');
+    precio.classList.add('precio');
+    precio.innerHTML = `Precio: <span>${PRICE}</span>`;
+
+    const precioMax = document.createElement('P');
+    precioMax.innerHTML = `Maximo: <span>${HIGHDAY}</span>`;
+
+    const precioMin = document.createElement('P');
+    precioMin.innerHTML = `Minimo: <span>${LOWDAY}</span>`;
+
+    const precioAct = document.createElement('P');
+    precioAct.innerHTML = `Actialización: <span>${CHANGEPCT24HOUR}%</span>`;
+
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioMax);
+    resultado.appendChild(precioMin);
+    resultado.appendChild(precioAct);
+}
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarSpinner() {
+    limpiarHTML();
+
+    const spinner = document.createElement('DIV');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `;
+
+    resultado.appendChild(spinner);
+}
